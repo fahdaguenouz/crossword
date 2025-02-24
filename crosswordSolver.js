04e07b790313f1c7f5aba8ea8711405b87e1ec14
fahdaguenouz <faguenouz@gmail.com>
2025-02-24 15:07:03 +0000

    finishing the words and puzzel verification

--- crosswordSolver.js
+++ crosswordSolver.js
@@ -1,20 +1,23 @@
 function CheckWords(word){
-    const regex=/[a-z]/g
+    const regex=/[^a-z]/gi
     
     if(!Array.isArray(word)||word.length==0){
         return true 
     }
     for(let i=0;i<word.length;i++){
-        if(word[i].length==0||typeof word[i]!="string"){
+        if(word[i].length<2||typeof word[i]!="string"){
             return true
         }
+
         for(let k=i+1;k<word[i].length;k++){
             if(word[k]==word[i]){
                 return true 
             }
         }
+        
         for(let j=0;j<word[i].length;j++){
-            if(!word[i][j].match(regex)){
+            //console.log(word[i][j]);
+            if(word[i][j].match(regex)!=null){
                 return true
             }
         }
@@ -24,41 +27,83 @@ function CheckWords(word){
 }
 
 function CheckPuzzel(puzzl){
-    const regex=/[0|2|1|.]/g
+    const regex=/[^0-2|.]/g
     if(typeof puzzl!="string"){
         return true 
     }
-    console.log(puzzl);
- for(let i=0;i<puzzl.length;i++){
-
-     if(!puzzl[i].match(regex)){
-         return true
-     }
- }
-
+    let res = puzzl.split("\n").join("")
+    if(res.match(regex)!=null)
+    {
+        return true
+    }
+    return false
 }
+
+function CheckWordInPuzzel(puzzel,words){
+    let wordlen=words.join("").length
+    // console.log(wordlen);
+    let puzz = puzzel.split("\n").join("")
+    puzz=puzz.split(".").join("")
+    let c=0
+    for(let i=0;i<puzz.length;i++){
+        c+=parseInt(puzz[i])+1
+    }
+    if(c!=wordlen){
+        return true 
+    }
+   return false
+
+  }
+
 
 function crosswordSolver(puzzleMap,words){
     let error =false
-    error = CheckWords(words)
-    error = CheckPuzzel(puzzleMap)
-    if(error){
+     if(CheckWords(words)||CheckPuzzel(puzzleMap)|| CheckWordInPuzzel(puzzleMap,words)){
         return console.log("Error");
-        
-    }else{
+     }
+    
+    
         return console.log("succ");
-    }
+    
 }
 
-// const emptyPuzzle=14
+// const puzzle=14
 
-const emptyPuzzle = `2001
-0..0
-1000
-0..0`
-const words = ['casa', 'alan', 'ciao',"anta"]
+// const puzzle = `2001
+// 0..0
+// 1000
+// 0..0`
+// const words = ["casa", 'alan', 'ciao',"anta"]
+// const puzzle = '2001\n0..0\n2000\n0..0'
+const puzzle = `...1...........
+..1000001000...
+...0....0......
+.1......0...1..
+.0....100000000
+100000..0...0..
+.0.....1001000.
+.0.1....0.0....
+.10000000.0....
+.0.0......0....
+.0.0.....100...
+...0......0....
+..........0....`
+const words = [
+  'sun',
+  'sunglasses',
+  'suncream',
+  'swimming',
+  'bikini',
+  'beach',
+  'icecream',
+  'tan',
+  'deckchair',
+  'sand',
+  'seaside',
+  'sandals',
+].reverse()
 //const words=["","hhh"]
-crosswordSolver(emptyPuzzle, words)
+crosswordSolver(puzzle, words)
 
 /* output:
 `casa
